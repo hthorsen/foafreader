7cd2762985fe6e71c6530f30560f39c2ac6ffec7
H. Thorsen <sul.thorsenh@DN0a23330b.SUNet>
2016-07-12 14:18:31 -0700

    abstract query

--- foaf_reader.rb
+++ foaf_reader.rb
@@ -7,9 +7,39 @@ graph = RDF::Graph.load("foaf.rdf")
 
 puts graph.inspect
 
-query ="SELECT * WHERE {?s ?p ?o}"
+query ="
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+SELECT DISTINCT ?o
+WHERE { ?s foaf:knows ?o}
+"
 
+puts "Before loading"
 sse = SPARQL.parse(query)
 sse.execute(graph) do |result|
 	puts result.o
+	triples = RDF::Resource(RDF::URI.new(result.o))
+	graph.load(triples)
+end
+
+puts "After loading"
+sse.execute(graph) do |result|
+	puts result.o
+end
+
+interest_query = "
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+SELECT DISTINCT ?o
+WHERE { ?s foaf:interest ?o }
+"
+
+tmp_query = "PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+PREFIX dbo: <http://dbpedia.org/ontology/>
+SELECT ?abs
+WHERE { ?s dbo:abstract ?abs
+FILTER (lang(?abs) = 'en')}"
+
+tmp_graph = RDF::Graph.load("http://dbpedia.org/resource/Quilting")
+sse_abstracts = SPARQL.parse(tmp_query)
+sse_abstracts.execute(tmp_graph) do |res|
+	puts res.abs
 end
